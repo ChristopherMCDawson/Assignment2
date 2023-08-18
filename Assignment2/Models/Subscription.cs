c5ea7c8aae868c463c96c60c0931afaa2725018d
Christopher Decariedawson <ChristopherMCDawson@gmail.com>
2023-08-18 11:13:00 -0400

    added missing Using statements and removed unneeded calls.

--- Assignment2/Models/Subscription.cs
+++ Assignment2/Models/Subscription.cs
@@ -1,11 +1,14 @@
-﻿namespace Assignment2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+
+
+namespace Assignment2.Models
 {
     public class Subscription
     {
         public int FanId { get; set; }
         public string SportClubId { get; set; }
 
-        public Fan Fan { get; set; }
-        public SportClub SportClub { get; set; }
     }
 }
